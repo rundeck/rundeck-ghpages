987c738910fd6887212b281a8bd9a10ee8d671ed
Greg Schueler <greg.schueler@gmail.com>
2011-10-11 12:02:23 -0700

    Remove unused javascript

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
 /* Author: 
 
 */
-(function(){
-    //modify links/text for current rundeck version
-    var rdvalue=function(name){
-        if(typeof(RDVERS)=="object" && typeof(RDVERS[name]) == 'string' && RDVERS[name] ){
-            return RDVERS[name];
-        }else{
-            return null;
-        }
-    };
-    //text
-    $(['version','jar_name','jar_sha']).each(function(x,prop){
-        if(rdvalue(prop)){
-            $('.rd_'+prop).each(function(){
-                $(this).text(RDVERS[prop]);   
-            });
-        }
-    });
-    //links
-    $(['jar_url','tarball','zipball','releasenotes']).each(function(x,prop){
-        if(rdvalue(prop)){
-            $('a.rd_'+prop).each(function(){
-                $(this).attr('href',RDVERS[prop]);
-            });
-        }
-    });
-}
-)();
